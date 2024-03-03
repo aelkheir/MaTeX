8b05c5f9a9a8af2128ded2044521db2eff3a825f
aelkheir <arelkheir@gmail.com>
2024-03-03 19:04:42 +0300

    Add crud operations (edit, delete) on Lesson.

--- src/renderer/routes/QuestionList.tsx
+++ src/renderer/routes/QuestionList.tsx
@@ -1,15 +1,18 @@
 import React, { useContext, useEffect, useMemo, useRef, useState } from "react";
 import {
   ActionFunctionArgs,
+  Form,
   Link,
   LoaderFunctionArgs,
   NavLink,
   redirect,
   useLoaderData,
   useLocation,
+  useParams,
+  useSubmit,
 } from "react-router-dom";
-import { useEditor, EditorContent } from "@tiptap/react";
-import { PlusIcon } from "@heroicons/react/24/solid";
+import { useEditor, EditorContent, generateText } from "@tiptap/react";
+import { ClipboardIcon, PlusIcon } from "@heroicons/react/24/solid";
 import { Document } from "@tiptap/extension-document";
 import { Text } from "@tiptap/extension-text";
 import { Paragraph } from "@tiptap/extension-paragraph";
@@ -27,7 +30,22 @@ import { FixedSizeList as List, ListChildComponentProps } from "react-window";
 import { useElementScrollRestoration } from "../hooks/useElementScrollRestoration";
 import { LabelLarge } from "../components/text/LabelLarge";
 import { DisplayLatex, InlineLatex } from "../components/tiptap/LatexNode";
-import { LabelMedium } from "../components/text/LabelMedium";
+import { Controller, RefCallBack, useForm } from "react-hook-form";
+import {
+  Button,
+  Dialog,
+  DialogTrigger,
+  FieldError,
+  Heading,
+  Input,
+  Label,
+  Modal,
+  ModalOverlay,
+  TextField,
+  Text as RACText,
+} from "react-aria-components";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 export async function action({ request, params }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -41,13 +59,8 @@ export async function action({ request, params }: ActionFunctionArgs) {
       break;
 
     case "DELETE":
-      if ("lessonId" in updates) {
-        await window.electron.deleteLesson(Number(updates.lessonId));
-        return redirect(`/units/${params.unitId}`);
-      } else if ("questionId" in updates) {
-        await window.electron.deleteQuestion(Number(updates.questionId));
-        return null;
-      }
+      await window.electron.deleteLesson(Number(updates.lessonId));
+      return redirect(`/courses/${params.courseId}/units/${params.unitId}`);
       break;
     case "PUT":
       const question = await window.electron.addQuestion(
@@ -132,13 +145,9 @@ export const QuestionList = () => {
       <div className="w-full col-start-3 row-start-1 row-span-2 flex flex-col bg-surface">
         <div className="w-full h-16 shrink-0 flex items-center px-4 justify-between space-x-8">
           <TipTapHeader lesson={lesson} />
-          <div className="flex space-x-2">
-            <button className="py-3 px-4 text-on-surface">
-              <LabelLarge>Delete</LabelLarge>
-            </button>
-            <button className="py-3 px-4 text-on-surface">
-              <LabelLarge>Edit</LabelLarge>
-            </button>
+          <div className="flex items-center space-x-2">
+            <EditLesson key={lesson.id} lesson={lesson} />
+            <DeleteLesson key={"delete-" + String(lesson.id)} />
           </div>
 
           {/* Edit Lesson Modal */}
@@ -163,7 +172,7 @@ export const QuestionList = () => {
             width={"w-full"}
             className="scrollbar-thin scrollbar-thumb-outline-variant"
             itemCount={lesson.questions.length}
-            itemSize={150}
+            itemSize={100}
             itemData={lesson.questions}
             overscanCount={2}
           >
@@ -272,6 +281,10 @@ const TipTapNav: React.FC<ListChildComponentProps<Question[]>> = ({
     []
   );
 
+  if (editor) {
+    console.log(editor.getHTML());
+  }
+
   return (
     <>
       <div style={style} className="w-full px-4 pb-2 bg-surface">
@@ -287,7 +300,7 @@ const TipTapNav: React.FC<ListChildComponentProps<Question[]>> = ({
             readOnly
             className={`h-full w-full overflow-clip text-on-surface `}
           />
-          <div className="w-full flex justify-end text-on-surface">
+          {/* <div className="w-full flex justify-end text-on-surface">
             <span className="">
               <button
                 className="px-3 py-2 flex items-center space-x-1"
@@ -299,9 +312,319 @@ const TipTapNav: React.FC<ListChildComponentProps<Question[]>> = ({
                 <LabelMedium>Delete</LabelMedium>
               </button>
             </span>
-          </div>
+          </div> */}
         </NavLink>
       </div>
     </>
+  );
+};
+
+const schema = yup.object({
+  name: yup.string().test({
+    name: "value",
+    skipAbsent: true,
+    test(value, ctx) {
+      if (value === "") {
+        return ctx.createError({ message: "Required" });
+      }
+      const text = generateText(JSON.parse(value), [
+        Document.extend({
+          content: "block",
+        }),
+        Text,
+        Paragraph,
+        InlineLatex,
+      ]);
+      if (text === "") {
+        return ctx.createError({ message: "Required" });
+      }
+      if (text.length > 0 && text.length <= 4) {
+        return ctx.createError({
+          message: "Name must be at least 5 characters long",
+        });
+      }
+      return true;
+    },
+  }),
+});
+
+export const TipTapInput = ({
+  content,
+  onChange,
+  contentRef,
+}: {
+  content: string;
+  onChange: (...event: unknown[]) => void;
+  contentRef: RefCallBack;
+}) => {
+  const editor = useEditor({
+    editorProps: {
+      attributes: {
+        class:
+          "p-2 bg-gray-300 grow outline-none border-b-2 border-transparent focus:border-primary",
+      },
+    },
+    extensions: [
+      Document.extend({
+        content: "block",
+      }),
+      Text,
+      Paragraph.configure({
+        HTMLAttributes: {
+          class: "h-full",
+        },
+      }),
+      InlineLatex,
+    ],
+    content: !content ? "<p></p>" : JSON.parse(content),
+    onUpdate({ editor }) {
+      onChange(JSON.stringify(editor.getJSON()));
+    },
+  });
+
+  useEffect(() => {
+    editor?.commands.focus("end");
+  }, [editor]);
+
+  return (
+    <div className="overflow-y-auto flex flex-col">
+      <EditorContent
+        editor={editor}
+        ref={contentRef}
+        className="grow w-full flex items-center overflow-y-auto text-on-surface"
+      />
+    </div>
+  );
+};
+
+const EditLesson = ({ lesson }: { lesson: Lesson }) => {
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      name: lesson.name,
+    },
+    resolver: yupResolver(schema),
+  });
+  const submit = useSubmit();
+  const formRef = useRef<HTMLFormElement>(null);
+
+  return (
+    <DialogTrigger>
+      <Button
+        className={`flex items-center justify-center text-sm font-normal p-1 pressed:bg-black/5`}
+      >
+        Edit
+      </Button>
+      <ModalOverlay
+        className={`
+          fixed inset-0 z-10 overflow-y-auto bg-black/25 flex min-h-full items-center justify-center p-4`}
+      >
+        <Modal
+          className={` w-full max-w-md overflow-hidden bg-white p-6 text-left align-middle`}
+        >
+          <Dialog role="dialog" className="outline-none relative">
+            {({ close }) => (
+              <>
+                <Heading
+                  slot="title"
+                  className="text-xxl font-semibold leading-6 my-0 text-slate-700"
+                >
+                  Edit Lesson
+                </Heading>
+                <Form
+                  ref={formRef}
+                  onSubmit={handleSubmit(() => {
+                    if (formRef.current) {
+                      submit(formRef.current, { method: "POST" });
+                      close();
+                    }
+                  })}
+                  className="mt-2"
+                >
+                  <Controller
+                    control={control}
+                    name="name"
+                    render={({
+                      field: { name, value, onChange, onBlur, ref },
+                      fieldState: { invalid, error },
+                    }) => (
+                      <TextField
+                        name={name}
+                        value={value}
+                        defaultValue={lesson.name}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        autoFocus
+                        isRequired
+                        // // Let React Hook Form handle validation instead of the browser.
+                        validationBehavior="aria"
+                        isInvalid={invalid}
+                        className={"flex flex-col"}
+                      >
+                        <Label>Name</Label>
+                        <TipTapInput
+                          content={value}
+                          onChange={onChange}
+                          contentRef={ref}
+                        />
+                        <Input ref={ref} hidden />
+                        <RACText slot="description">
+                          You can write inline LaTeX using{" "}
+                          <span className="italic">
+                            ${"<"}latex_code{">"}$
+                          </span>
+                        </RACText>
+                        <FieldError>{error?.message}</FieldError>
+                      </TextField>
+                    )}
+                  />
+                  <div className="mt-6 flex justify-end gap-2">
+                    <Button
+                      onPress={close}
+                      className={`inline-flex justify-center border border-solid border-transparent px-5 py-2 font-semibold font-[inherit] text-base transition-colors cursor-default outline-none focus-visible:ring-2 ring-blue-500 ring-offset-2 bg-slate-200 text-slate-800 hover:border-slate-300 pressed:bg-slate-300`}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      type="submit"
+                      className={`inline-flex justify-center border border-solid border-transparent px-5 py-2 font-semibold font-[inherit] text-base transition-colors cursor-default outline-none focus-visible:ring-2 ring-blue-500 ring-offset-2 bg-red-500 text-white hover:border-red-600 pressed:bg-red-600`}
+                    >
+                      Edit
+                    </Button>
+                  </div>
+                </Form>
+              </>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
+  );
+};
+
+const DeleteLesson = () => {
+  const { handleSubmit, control } = useForm({
+    defaultValues: {
+      confirmation: "",
+    },
+  });
+  const params = useParams();
+  const formRef = useRef<HTMLFormElement>(null);
+  const submit = useSubmit();
+
+  return (
+    <DialogTrigger>
+      <Button className="flex items-center justify-center text-sm font-normal p-1 pressed:bg-black/5">
+        Delete
+      </Button>
+      <ModalOverlay
+        className={`
+          fixed inset-0 z-10 overflow-y-auto bg-black/25 flex min-h-full items-center justify-center p-4`}
+      >
+        <Modal
+          className={`w-full max-w-md overflow-hidden bg-white p-6 text-left align-middle`}
+        >
+          <Dialog role="dialog" className="outline-none relative">
+            {({ close }) => (
+              <>
+                <Heading
+                  slot="title"
+                  className="text-xxl font-semibold leading-6 my-0 text-slate-700"
+                >
+                  Delete lesson
+                </Heading>
+                <Form
+                  ref={formRef}
+                  onSubmit={handleSubmit(() => {
+                    const formData = new FormData();
+                    formData.append("lessonId", String(params.lessonId));
+                    if (formRef.current) {
+                      submit(formData, { method: "DELETE" });
+                    }
+                  })}
+                  className="mt-2"
+                >
+                  <Controller
+                    control={control}
+                    name="confirmation"
+                    rules={{
+                      required: "Cannot be empty",
+                      pattern: {
+                        value: /^I am absolutely sure\.$/,
+                        message: "Text does not match",
+                      },
+                    }}
+                    render={({
+                      field: { name, value, onChange, onBlur, ref },
+                      fieldState: { invalid, error },
+                    }) => (
+                      <TextField
+                        name={name}
+                        value={value}
+                        onChange={onChange}
+                        onBlur={onBlur}
+                        isRequired
+                        autoFocus
+                        // Let React Hook Form handle validation instead of the browser.
+                        validationBehavior="aria"
+                        isInvalid={invalid}
+                        className={"flex flex-col"}
+                      >
+                        <Label>Confirmation</Label>
+                        <Input
+                          ref={ref}
+                          className={
+                            "p-2 bg-gray-300 grow outline-none border-b-2 border-transparent focus:border-primary"
+                          }
+                        />
+                        <div className="flex justify-between">
+                          <RACText slot="description">
+                            To confirm type:{" "}
+                            <span className="italic font-medium">
+                              I am absolutely sure.
+                            </span>
+                          </RACText>
+                          <Button
+                            aria-label="Copy to clipboard"
+                            type="button"
+                            className="p-1"
+                            onPress={() => {
+                              const type = "text/plain";
+                              const blob = new Blob(["I am absolutely sure."], {
+                                type,
+                              });
+                              const data = [
+                                new ClipboardItem({ [type]: blob }),
+                              ];
+                              navigator.clipboard.write(data);
+                            }}
+                          >
+                            <ClipboardIcon className="w-4 h-4 text-outline cursor-pointer" />
+                          </Button>
+                        </div>
+                        <FieldError>{error?.message}</FieldError>
+                      </TextField>
+                    )}
+                  />
+                  <div className="mt-6 flex justify-end gap-2">
+                    <Button
+                      onPress={close}
+                      className={`inline-flex justify-center border border-solid border-transparent px-5 py-2 font-semibold font-[inherit] text-base transition-colors cursor-default outline-none focus-visible:ring-2 ring-blue-500 ring-offset-2 bg-slate-200 text-slate-800 hover:border-slate-300 pressed:bg-slate-300`}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      type="submit"
+                      className={`inline-flex justify-center border border-solid border-transparent px-5 py-2 font-semibold font-[inherit] text-base transition-colors cursor-default outline-none focus-visible:ring-2 ring-blue-500 ring-offset-2 bg-red-500 text-white hover:border-red-600 pressed:bg-red-600`}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Form>
+              </>
+            )}
+          </Dialog>
+        </Modal>
+      </ModalOverlay>
+    </DialogTrigger>
   );
 };
